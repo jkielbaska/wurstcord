e184527c9769d7629ef1196a5b5ca8659c8265ce
Jakub <kielbaskakuba@gmail.com>
2023-10-12 22:05:52 +0200

    Style landing page

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,15 @@
 import { SignIn } from "@clerk/nextjs";
+import { SignInButton } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    // <SignIn/>
+    <div className="w-full flex justify-center">
+      <SignInButton mode="modal">
+        <button className="bg-red-700 text-sm font-bold tracking-tight rounded-sm uppercase border-2 border-white m-2 w-2/4 max-w-[330px] h-9 max-h-9 hover:animate-wiggle">
+          Sign in
+        </button>
+      </SignInButton>
+    </div>
+  );
 }
--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,28 @@
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-full flex items-center justify-center">{children}</div>
+    <>
+      <h1 className="w-full flex justify-center uppercase text-5xl p-5">
+        wurstcord
+      </h1>
+      <div className="pl-5">
+        <p className="text-2xl">welcome!</p>
+        <ul className="p-5">
+          <li>
+            * if you have invite code you have to sign in or sign up first, then
+            paste your invite link into browser.{" "}
+          </li>
+          <li className="pt-5">
+            * if you want to create your own server - just sign in/sign up!
+          </li>
+        </ul>
+      </div>
+      <div className="flex flex-col items-center justify-center h-2/4">
+        <p className="p-5 text-center">
+          remember - you can always delete your server or account
+        </p>
+        {children}
+      </div>
+    </>
   );
 };
 
